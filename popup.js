cc8633a45d6a477f08bfbf636dc11f0bf1caa834
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-07-19 21:04:54 +0100

    Make error corrections to js file

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ const popUps = [
     image: './images/SnapshootPortfolio.png',
     description:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.",
-    technologies: ['Css', 'Html', 'Bootstrap',],
+    technologies: ['Css', 'Html', 'Bootstrap'],
     live_link: 'https://promzzykoncepts.github.io/My-Portfolio/',
     source_link: 'https://github.com/promzzykoncepts/my-Portfolio/',
   },
@@ -70,9 +70,7 @@ const popUps = [
   },
 ];
 
-
 function popUpDiv(id) {
-
   // here i created the necessary divs and classes
 
   const mainDiv = document.createElement('div');
@@ -134,3 +132,4 @@ function popUpDiv(id) {
   mainDiv.appendChild(divSourceBtn);
   document.body.appendChild(mainDiv);
 }
+popUpDiv();
